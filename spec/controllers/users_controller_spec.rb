3125353708b9652b845385915ba0aac92059ebd1
Shingo <ninjashingo@gmail.com>
2012-02-20 15:43:33 -0600

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -83,6 +83,12 @@ describe UsersController do
 	   post :create, :user => @attr
 	   flash[:success].should =~ /welcome to the sample app/i
 	end
+
+	it "should sign the user in" do
+	   post :create, :user => @attr
+	   controller_should be_signed_in
+	end
+
       end
 
   end
